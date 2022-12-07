ad6e724c82c64cc3ffada2be71bd63127002bbca
Ruslanexe <ruslangurskij5@gmail.com>
2022-12-07 09:57:28 +0200

    Finish storage section

--- src/Components/StorageSection/StorageSection.jsx
+++ src/Components/StorageSection/StorageSection.jsx
@@ -4,6 +4,7 @@ import storagebottom from "../../Images/storagebottom.svg";
 import house from "../../Images/grain_house.svg";
 import dangerous_icon from "../../Images/dangerous_icon.svg";
 import notification from "../../Images/notif_icon.svg";
+import plus from "../../Images/plus.svg";
 export function StorageSection() {
   return (
     <div className="storagesection">
@@ -11,26 +12,52 @@ export function StorageSection() {
         <p className="username-hi">Hello bread</p>
         <p className="yourstorages">Your Storages</p>
       </div>
-      <div className="storage">
-        <img
-          className="notification"
-          src={notification}
-          alt="notification...."
-        />
-        <p className="storage-name">Ivan</p>
-        <img
-          className="dangerous_icon"
-          src={dangerous_icon}
-          alt="dangerous_icon...."
-        />
-        <div className="storage-photo">
-          <img src={house} alt="house...." />
+      <div className="storages">
+        <div className="storage">
+          <img
+            className="notification"
+            src={notification}
+            alt="notification...."
+          />
+          <p className="storage-name">Ivan</p>
+          <img
+            className="dangerous_icon"
+            src={dangerous_icon}
+            alt="dangerous_icon...."
+          />
+          <div className="storage-photo">
+            <img src={house} alt="house...." />
+          </div>
+          <img
+            className="bottom-side"
+            src={storagebottom}
+            alt="storagebottom....."
+          />
         </div>
-        <img
-          className="bottom-side"
-          src={storagebottom}
-          alt="storagebottom....."
-        />
+        <div className="storage">
+          <img
+            className="notification"
+            src={notification}
+            alt="notification...."
+          />
+          <p className="storage-name">Illya</p>
+          <img
+            className="dangerous_icon"
+            src={dangerous_icon}
+            alt="dangerous_icon...."
+          />
+          <div className="storage-photo">
+            <img src={house} alt="house...." />
+          </div>
+          <img
+            className="bottom-side"
+            src={storagebottom}
+            alt="storagebottom....."
+          />
+        </div>
+        <div className="storage-add">
+          <img className="plus" src={plus} />
+        </div>
       </div>
     </div>
   );
